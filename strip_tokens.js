8d746a8004bdfb3d9e2fc9f20cc9f0894a1e656b
ardaerturk <arda.erturk@mail.utoronto.ca>
2018-01-05 13:13:40 -0500

    Update strip_tokens.js

--- strip_tokens.js
+++ strip_tokens.js
@@ -4,8 +4,8 @@ const configPath = './modules_config/botpress-messenger.json'
 
 let content = JSON.parse(fs.readFileSync(configPath))
 
-content.applicationID = ''
-content.accessToken = ''
+content.applicationID = '626012380748827'
+content.accessToken = 'EAAI5Wt2UXBsBADCJ58biLMSzV9VxIHhtZC913ailzn2xCZCPsPj5H51T0ZAXJZAgl7WIAhsN1XS44gEqWetcJ9EWcEFXZC2kRGgL6hAtjv3kpZBLWEiRFLsfYKeAXZAoZCkfdqD0c9l15ZCQeGWEYIRphYkcSePlx2BRonJpXth7QRgZDZD'
 content.error = ''
 content.message = ''
 content.verifyToken = '1234567890'
