d0c33d6e26ba572f844901fe73a81698b90a17e5
ardaerturk <arda.erturk@mail.utoronto.ca>
2018-01-10 11:25:08 -0500

    Update index.js

--- index.js
+++ index.js
@@ -143,10 +143,12 @@ module.exports = function(bp) {
         bp.messenger.sendText(id, "Maalesef artik Arda birebir danismanlik ve gorusme icin vakit bulamiyor. Ben elimden geleni yapiyorum. Emin olabilirsin. Yine de icin rahatlasin diye soyluyorum, Arda bu konusmalarimizi inceleyecek. Onun cevaplamasi gereken bir sey olursa mutlaka gorur.", { typing: true, waitDelivery: true })
     } else if (event.konu.includes("basarilar") || event.konu.includes("tebrikler") || event.konu.includes("tesekkur") || event.konu.includes("tesekkurler")) {
         bp.messenger.sendText(id, "Guzel dileklerin icin cok tesekkur ederim. Sana da basarilar diliyorum.", { typing: true, waitDelivery: true })
+    } else if (event.konu.includes("nasilsin") || event.konu.includes("naber")) {
+        bp.messenger.sendText(id, "Iyiyim, tesekkurler " + first_name ". Sen nasilsin?", { typing: true, waitDelivery: true })
+    } else if (event.konu == "." || event.konu.includes("iyi")) {
+        bp.messenger.sendText(id, "👍", { typing: true, waitDelivery: true })
     } else if ((event.konu.includes("merhaba") || event.konu.includes("salam") || event.konu.includes("meraba") || event.konu.includes("selamun")  || event.konu.includes("selam") || event.konu.includes("maraba")) &&  (event.konu.length < 13)) {
-        bp.messenger.sendText(id, 'Merhaba ' + event.user.first_name + ". Bugun hangi konuda fikir edinmek istersin?", { typing: true, waitDelivery: true })
-    //} else if (event.konu.includes("merhaba") || event.konu.includes("danismanlik")) {
-        //bp.messenger.sendText(id, "Maalesef artik Arda birebir danismanlik ve gorusme icin vakit bulamiyor. Ben elimden geleni yapiyorum. Emin olabilirsin. Yine de icin rahatlasin diye soyluyorum, Arda bu konusmalarimizi inceleyecek. Onun cevaplamasi gereken bir sey olursa mutlaka gorur.", { typing: true, waitDelivery: true })
+       bp.messenger.sendText(id, 'Merhaba ' + event.user.first_name + ". Bugun hangi konuda fikir edinmek istersin?", { typing: true, waitDelivery: true })
     } else {
         const text = _.sample(DEFAULT_ANSWERS(event))
           return bp.messenger.sendText(event.user.id, text, pickCategory)
